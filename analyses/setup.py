1879185dad634a2d5891620df830469035af4956
marcelnoehre <marcelnoehre@web.de>
2023-11-19 00:08:15 +0100

    optimize airports script

--- analyses/setup.py
+++ analyses/setup.py
@@ -12,7 +12,7 @@ districts = {
                   62, 63, 65, 66, 67, 71, 72, 76, 77, 80, 85, 89, 91, 97, 106, 108, 111, 112, 123,
                   133, 149, 150, 154, 154, 155, 165, 177, 178, 181, 188, 189, 190, 195, 210, 217,
                   222, 225, 227, 228, 255, 256, 257],
-    'manhatten': [4, 12, 13, 24, 41, 42, 43, 45, 48, 50, 68, 74, 75, 79, 87, 88, 90, 100, 103, 104,
+    'manhattan': [4, 12, 13, 24, 41, 42, 43, 45, 48, 50, 68, 74, 75, 79, 87, 88, 90, 100, 103, 104,
                   105, 107, 113, 114, 116, 120, 125, 127, 128, 137, 140, 141, 142, 143, 144, 148,
                   151, 152, 153, 158, 161, 162, 163, 164, 166, 170, 186, 194, 202, 209, 211, 224,
                   229, 230, 231, 232, 233, 234, 236, 237, 238, 239, 243, 244, 246, 249, 261, 262,
@@ -28,7 +28,7 @@ districts = {
 airports = [132, 138]
 
 month_labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-district_labels = ['Bronx', 'Brooklyn', 'Manhatten', 'Queens', 'Staten Island']
+district_labels = ['Bronx', 'Brooklyn', 'Manhattan', 'Queens', 'Staten Island']
 airport_labels = ['JFK Airport', 'LaGuardia Airport']
 tip_labels = ['average', 'lower', 'upper']
 
